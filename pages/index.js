856f4c2c6b0441318849c10887f3f1c15d595b3b
NALASKY <33963608+NALASKY@users.noreply.github.com>
2019-05-05 16:45:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
-import Link from "next/link";
-import Header from "../components/header";
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
 
-function Index() {
+function PostLink(props) {
   return (
-    <main>
-      <Header />
-      <section>
-        <Link href="/about">
-          <a>Go to About Me</a>
-        </Link>
-      </section>
-    </main>
-  );
+    <li>
+      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
 }
 
-export default Index;
+export default function Index() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  )
+}
