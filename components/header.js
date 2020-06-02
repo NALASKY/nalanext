b7628abe6652f2c034e714859b44c2361bed078f
NALASKY <33963608+NALASKY@users.noreply.github.com>
2020-06-02 14:09:29 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,6 +2,7 @@ function Header() {
   return (
     <header>
       <h1>Nalasky <==></h1>
+    <script data-ad-client="ca-pub-9670879980399014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     </header>
   );
 }
