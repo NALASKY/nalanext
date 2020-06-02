192782fd67903bc5591bb456d97789dbfcaccf68
NALASKY <33963608+NALASKY@users.noreply.github.com>
2020-06-02 14:08:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default () => (
     <section>
       <Link href="/about">
         <a>Go to About Me</a>
+  <script data-ad-client="ca-pub-9670879980399014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Link>
     </section>
   </main>
